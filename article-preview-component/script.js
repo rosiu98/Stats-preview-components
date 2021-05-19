fa4dc592cee8ccdc970d457228bb637c2a680867
Paweł <rosiu978@gmail.com>
2021-05-20 01:45:46 +0200

    js done

--- article-preview-component/script.js
+++ article-preview-component/script.js
@@ -1,7 +1,21 @@
 const display = document.querySelector(".display");
-const link = document.querySelector(".link")
-
+const link = document.querySelector(".link");
+const info = document.querySelector(".info");
+const btn = document.querySelector("#buttonClose");
+const mobile = document.querySelector(".display-mobile");
+const click = document.querySelector(".click")
 
 link.addEventListener("click", () => {
     display.classList.toggle("hidden");
+})
+
+btn.addEventListener("click", () => {
+    info.classList.toggle("hidden");
+    mobile.classList.toggle("hidden");
+
+})
+
+click.addEventListener("click", () => {
+    info.classList.toggle("hidden");
+    mobile.classList.toggle("hidden");
 })
